55fce024606c8968b732257fcd1fda437764454e
Brendan Ball <ball.brendan50@gmail.com>
2017-06-14 08:00:14 +0200

    some router changes

--- src/main/react/src/AppRouter.js
+++ src/main/react/src/AppRouter.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {
     BrowserRouter as Router,
-    Route
+    Route,
+    Link
 } from 'react-router-dom'
 
 import Home from './Home.js'
@@ -9,23 +10,21 @@ import About from './About.js'
 
 import AppBar from 'react-toolbox/lib/app_bar';
 import Navigation from 'react-toolbox/lib/navigation';
-import Link from 'react-toolbox/lib/link';
 
 class AppRouter extends Component {
     render() {
         return (
             <Router>
                 <div>
-                    <AppBar title='React Toolbox'>
+                    <AppBar title='Giff Meds'>
                         <Navigation type='horizontal'>
-                            <Link href='/' label="Home" />
-                            <Link href='/about' label="About" />
+                            <Link to='/'>Home</Link>
+                            <Link to='/about'>About</Link>
                         </Navigation>
                     </AppBar>
-                    <div className="App">
-                        <Route exact path="/" component={Home}/>
-                        <Route path="/about" component={About}/>
-                    </div>
+
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/about" component={About}/>
                 </div>
             </Router>
         );
