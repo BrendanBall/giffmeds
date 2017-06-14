254a550b1fe19cfdd51ec3be8386fd74c5d4158d
Ntokozo Zwane <ntkzwane@gmail.com>
2017-06-14 11:45:13 +0200

    Merged branch origin/master into 'master'

---
+++ deploy-dapp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -s $PWD/src/main/react/build $HOME/.local/share/io.parity.ethereum/dapps/giffmeds
+
--- src/main/react/src/AppRouter.js
+++ src/main/react/src/AppRouter.js
@@ -5,8 +5,9 @@ import {
     Link
 } from 'react-router-dom'
 
-import Home from './Home.js'
-import About from './About.js'
+import Home from './Home'
+import About from './About'
+import Prescriptions from './Prescriptions'
 
 class AppRouter extends Component {
     render() {
@@ -16,12 +17,14 @@ class AppRouter extends Component {
                     <h1>Giff Meds</h1>
                     <lu>
                         <li>
-                            <Link to="/">Home</Link>
+                            <Link to="/">Prescriptions</Link>
+                        </li>
+                        <li>
                             <Link to="/about">About</Link>
                         </li>
                     </lu>
 
-                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/" component={Prescriptions}/>
                     <Route path="/about" component={About}/>
                 </nav>
             </Router>
