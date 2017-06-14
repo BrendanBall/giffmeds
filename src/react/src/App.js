8526fa8c6fc471a1ece138391263704fe674d790
Brendan Ball <ball.brendan50@gmail.com>
2017-06-14 12:00:43 +0200

    fixed some stuff

--- src/react/src/App.js
+++ src/react/src/App.js
@@ -5,7 +5,7 @@ import AppRouter from './AppRouter.js';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <AppRouter />
       </div>
     );
